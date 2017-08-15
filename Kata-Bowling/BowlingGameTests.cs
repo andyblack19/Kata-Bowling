95f844cdc00a01343ac1ce11fead006953c8837c
Andy Black <andyblack16@gmail.com>
2017-08-15 13:07:22 +0100

    Adds support for rolling all gutter balls

---
+++ Kata-Bowling/BowlingGameTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace Kata_Bowling
+{
+    public class BowlingGameTests
+    {
+        [Test]
+        public void Game_score_is_zero_when_no_pins_hit()
+        {
+            var game = new BowlingGame();
+            RollTwentyZeros(game);
+            Assert.That(game.Score(), Is.EqualTo(0));
+        }
+
+        private static void RollTwentyZeros(BowlingGame game)
+        {
+            for (var i = 0; i < 20; i++)
+                game.Roll(0);
+        }
+    }
+
+    public class BowlingGame
+    {
+        public void Roll(int pins)
+        {
+        }
+
+        public int Score()
+        {
+            return 0;
+        }
+    }
+}
